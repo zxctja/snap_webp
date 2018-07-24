17c44529c326297f76862f502623212df2427be5
zxctja <zxctja@163.com>
2018-07-24 13:58:46 +0800

    time

--- src/enc/webp_enc.c
+++ src/enc/webp_enc.c
@@ -410,7 +410,7 @@ int WebPEncode(const WebPConfig* config, WebPPicture* pic) {
 	clock_gettime(CLOCK_REALTIME, &time_start);
 	clock_gettime(CLOCK_REALTIME, &time_end);
 	fprintf(stdout, "%lluns\n", (long long)((double)((time_end.tv_sec-time_start.tv_sec)*1000000000+(time_end.tv_nsec-time_start.tv_nsec))));
-	pthread_mutex_unlock(&impl->mutex_);
+	pthread_mutex_unlock(mutex_);
 
 
 //	struct timespec time_start={0, 0},time_end={0, 0};
